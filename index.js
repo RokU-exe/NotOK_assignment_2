9c320595228f7193c643a7312aacdf761a6981b8
RokU <104127134+RokU-exe@users.noreply.github.com>
2023-05-12 23:00:06 +0700

    new update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 const Product = require('./model/Product');
 
 //render home page
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     Product.find()
     .then((products) => {
         res.render('home', {products: products});
@@ -20,25 +20,7 @@ app.get('/home', (req, res) => {
     .catch((error) => console.log(error.message));
 });
 
-//render login page
-app.get('/login', (req, res) => {
-    Product.find()
-    .then((products) => {
-        res.render('login', {products: products});
-    })
-    .catch((error) => console.log(error.message));
-});
-
-//render product page
-app.get('/product', (req, res) => {
-    Product.find()
-    .then((products) => {
-        res.render('product', {products: products});
-    })
-    .catch((error) => console.log(error.message));
-});
-
-//render customer home page
+//render customer home page and get all products
 app.get('/customer-home', (req, res) => {
     Product.find()
     .then((products) => {
@@ -46,6 +28,20 @@ app.get('/customer-home', (req, res) => {
     })
     .catch((error) => console.log(error.message));
 });
+
+//get product based on their ID to view product detail pages
+app.get('/product/:id', (req, res) => {
+    Product.findById(req.params.id)
+    .then((product) => {
+      if (!product) {
+        return res.send("Cannot found that ID!");
+      }
+      res.render('product', {product: product});
+    })
+    .catch((error) => res.send(error));
+});
+
+
 
 
 app.listen(port, () => {
